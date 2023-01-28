ce6e04e31cecc49cb38edf009e0b4d08ddd636c9
tanmay-sketch <56361307+tanmay-sketch@users.noreply.github.com>
2023-01-28 17:12:06 -0500

    Changes to README and uploadvideo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# spartahack-deadlift
+# Squat and Deadlift Computer Vision: SpartaHack
+
+This project aims to use computer vision techniques to analyze and improve squat and deadlift form.
+
+# Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+* Streamlit
+* Mediapipe
+* OpenCV
+* NumPy
+
--- pages/uploadvideo.py
+++ pages/uploadvideo.py
@@ -10,3 +10,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.join(__file__, '../../')))
 sys.path.append(BASE_DIR)
 
 from utils import get_media_pipe_pose
+from process_frame import ProcessFrame
+from thresholds import get_thresholds_beginner, get_thresholds_pro
+
+st.title("Squat Analysis")
+
+mode = st.radio('Select Radio',['Beginner','Pro'], horizontal=True)
+
+thresholds = None
+
+if mode == 'Beginner': 
+    thresholds = get_thresholds_beginner()
+
+elif mode == 'Pro':
+    thresholds = get_thresholds_pro()
